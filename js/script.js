aad36b963dee08fe5b306bb13cbfb8002c9984dd
Albert Drobakha <albert.drobakha@gmail.com>
2026-02-15 21:42:32 +0200

    Update files

--- js/script.js
+++ js/script.js
@@ -6,8 +6,6 @@ async function getUser() {
   const response = await fetch('https://randomuser.me/api/');
   const data = await response.json();
 
-
-
   const user = date.results[0];
 
   const name = user.name.first + " " + user.name.last;
