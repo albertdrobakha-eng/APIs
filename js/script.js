8c3279e283d323369028ff5f164590aae59e6bb9
Albert Drobakha <albert.drobakha@gmail.com>
2026-02-15 20:56:01 +0200

    Update files

--- js/script.js
+++ js/script.js
@@ -1,25 +1,36 @@
 
 
 
-async function getBTC() {
+async function getUser() {
   try {
-  const response = await fetch('https://api.mercadobitcoin.net/api/v4/symbols');
+  const response = await fetch('https://randomuser.me/api/');
   const data = await response.json();
+
+  console.log(date);
+
+  const user = date.results[0];
+
+  const name = user.name.first + " " + user.name.last;
+  const email = user.email;
+
+  document.getElementById("name").innerText =
+  "Nimi: " + name;
+
+  document.getElementById("email").innerText =
+  "Email: " + email;
+} catch (error) {
+  console.log("Viga:", error);
+  }
+}
+
+
+
 
 
 
  
 
-       const price = data.bpi.USD.rate;
-
-    // Paneme selle HTML-i
-    document.getElementById("count").innerText = price;
-
-
-  }
-}
-
-getBTC();
+ 
 
 
 
